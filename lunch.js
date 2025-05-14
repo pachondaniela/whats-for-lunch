ae2e7c36fcc4958aa50e7cefc5cac040ba7b31d3
Daniela Pachon <[pachon.daniela@gmail.com]>
2025-05-13 21:03:43 -0400

    Some important context here

--- lunch.js
+++ lunch.js
@@ -14,7 +14,15 @@
  */
 
 const whatToDoForLunch = function(hungry, availableTime) {
-  console.log("I don't know what to do!");
+  if(hungry === false) {
+    console.log("Get back to work!")
+  } else if (hungry === true && availableTime < 20){
+    console.log("Pick something up and eat it in the lab")
+  } else if (hungry === true && availableTime < 30) {
+    console.log("Try a place near by")
+  } else if (hungry=== true && availableTime > 30) {
+    console.log("Remember you are in a bootcamp with not much time to spare")
+  }
 }
 
 
@@ -25,15 +33,15 @@ const whatToDoForLunch = function(hungry, availableTime) {
 
 console.log("I'm hungry and I have 20 minutes for lunch.");
 whatToDoForLunch(true, 20);
-console.log("---");
+// console.log("---");
 
 console.log("I'm hungry and I have 50 minutes for lunch.");
 whatToDoForLunch(true, 50);
-console.log("---");
+// console.log("---");
 
 console.log("I'm not hungry and I have 30 minutes for lunch.");
 whatToDoForLunch(false, 30);
-console.log("---");
+// console.log("---");
 
 console.log("I'm hungry and I have 15 minutes for lunch.");
 whatToDoForLunch(true, 15);
